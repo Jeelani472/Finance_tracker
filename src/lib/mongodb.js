ee5ea5624f5cf699c415e02d908e648c6c9ef627
Mulla Abdul Khader Jeelani <jeelanmohammed529@gmail.com>
2025-04-16 19:27:46 +0530

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,18 +1,21 @@
 import { MongoClient } from 'mongodb';
-const MONGODB_URI ='mongodb://localhost:27017/personal_finance';
-const uri =MONGODB_URI;
+
+const uri = process.env.MONGODB_URI;
+
 if (!uri) throw new Error('Missing MONGODB_URI in .env.local');
 
 let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
+  // In dev, use a global variable so it doesn't create multiple connections
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, just connect
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
